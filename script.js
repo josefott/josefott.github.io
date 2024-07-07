172cc12533be95448569817d6f3ed2b54c5cbc3f
Josef Ott <32463073+josefott@users.noreply.github.com>
2024-07-07 14:30:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
     const menu = document.getElementById('menu');
-    const menuToggle = document.getElementById('menuToggle')
+    const menuToggle = document.getElementById('menuToggle');
 
     // Dark mode toggle
     darkModeToggle.addEventListener('click', () => {
