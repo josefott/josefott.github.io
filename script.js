13e23db7c20925426d61f7241b4de0a0e7cf4965
Josef Ott <32463073+josefott@users.noreply.github.com>
2024-07-07 14:24:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const darkModeToggle = document.getElementById('darkModeToggle');
     const body = document.body;
+    const menu = document.getElementById('menu');
+    const menuToggle = document.getElementById('menuToggle')
 
     // Dark mode toggle
     darkModeToggle.addEventListener('click', () => {
@@ -11,5 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Check for saved dark mode preference
     if (localStorage.getItem('darkMode') === 'true') {
         body.classList.add('dark-mode');
+    }
+
+    menuToggle.addEventListener('click', () => {
+        if (menu.classList.contains('show')) {
+            menu.classList.remove('show');
+        } else {
+            menu.classList.add('show');
+        }
     }
 });
